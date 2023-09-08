59926b1bb1777e6595d49762a73a4650d7f7705f
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-09-08 21:54:06 +0100

    add validation

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -22,10 +22,16 @@ export async function action({ request }) {
     const formData = await request.formData()
     const noteData = Object.fromEntries(formData)
 
+    if (noteData.title.trim().length < 5) {
+        return { message: "Invalid title - must be at least 5 charachters long." }
+    }
+
     const existingNotes = await getStoredNotes()
     noteData.id = new Date().toISOString();
     const updatedNotes = existingNotes.concat(noteData)
     await storeNotes(updatedNotes)
+
+    // await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000))
 
     return redirect('/notes')
 }
